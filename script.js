c426189878e494ede2d14d93359029a9afbd58df
Nomenjanahary Clopedia <clopedia.nom@onja.org>
2020-08-01 16:11:56 +0300

    Worked with the form for a modal and its submit button.

--- script.js
+++ script.js
@@ -153,3 +153,148 @@ window.addEventListener('keydown', (e) => {
 })
 
 
+
+// Code for taking back the form from the onja-cookbook projects.
+
+const addButton = document.querySelector('.add_button');
+
+const handleAddBttn = (event) => {
+	innerModal.innerHTML = `
+	<form>
+		<fieldset>
+			<label for="recipe-name">What's the recipe name?</label>
+			<input type="text" id="recipe-name" name="title">
+		</fieldset>
+
+		<fieldset>
+			<label for="picture">Picture of the result (URL)</label>
+			<input type="url" id="picture" name="picture">
+		</fieldset>
+
+		<fieldset>
+			<label for="cook-name">Who is the cook?</label>
+			<input type="text" id="cook-name" name="author">
+		</fieldset>
+
+		<fieldset>
+			<label for="difficulty">What's the difficulty?</label>
+			<select id="difficulty" name="difficulty">
+				<option value="easy">Easy</option>
+				<option value="medium">Medium</option>
+				<option value="difficult">Difficult</option>
+			</select>
+		</fieldset>
+
+		<fieldset>
+			<label for="time">How much time does it take?</label>
+			<select id="time" name="timing">
+				<option value="15">Around 15min</option>
+				<option value="20">More than 15min</option>
+				<option value="30">About 30min</option>
+				<option value="45">Maybe 45min</option>
+				<option value="60">An hour or more</option>
+			</select>
+		</fieldset>
+
+		<fieldset>
+			<label for="ingredients">The ingredients list.</label>
+			<ul id="ingredientsList">
+				<li>
+					<input type="text" id="ingredients" name="ingredients" value="ingredient1">
+				</li>
+			</ul>
+			<div class="ingredients-button-container">
+				<button type="button" class="add-ingredients-button">Add a new ingredient to the list</button>
+			</div>
+		</fieldset>
+
+		<fieldset>
+			<label for="steps">The steps to make it.</label>
+			<ul id="stepsList">
+				<li>
+					<input type="text" id="steps" name="steps" value="step1">
+				</li>
+			</ul>
+			<div class="step-button-container">
+				<button type="button" class="add-step-button">Add a new step to the list</button>
+			</div>
+		</fieldset>
+
+		<div>
+			<button type="submit" class="submitbttn">Submit</button>
+		</div>
+
+	</form>
+	`;
+	outerModal.classList.add('open');
+};
+
+// An event listen to add the form in a modal.
+addButton.addEventListener('click', handleAddBttn);
+
+
+// A function for handling the submit button of the form.
+const handleSubmit = (event) => {
+	event.preventDefault();
+	if (event.target.matches('form')) {
+		const form = event.target;
+		const title = form.title.value;
+		const image = form.picture.value;
+		const cook = form.author.value;
+		const struggle = form.difficulty.value;
+		const period = form.timing.value;
+		const ingredients = form.ingredients.value;
+		const steps = form.steps.value;
+
+		const newCard = `
+		<div class="parent-element" data-id="${recipes.id}">
+			<h1>${title}</h1>
+			<div><img src="${image}"></div>
+			<ul>
+				<li>Timing: ${period}min</li>
+				<li>Difficulty: ${struggle}</li>
+			</ul>
+			<button type="button" class="more-button">More info</button>
+		</div>
+		`;
+
+		container.innerHTML += newCard;
+		outerModal.classList.remove('open');
+		form.reset();
+	}
+}
+window.addEventListener('submit', handleSubmit);
+
+// An event listener for handling the add more ingredients button.
+
+const handleAddIngrBttn = (event) => {
+	if (event.target.matches('button.add-ingredients-button')) {
+		const newIngredient = event.target;
+		const newIngr = `
+		<li>
+			<input id="ingredient" type="text" id="ingredients" name="ingredients">
+		</li>
+		`;
+
+		newIngredient.insertAdjacentHTML('beforebegin', newIngr);
+	}
+};
+
+window.addEventListener('click', handleAddIngrBttn);
+
+
+// An event listener for handling the add more steps button.
+const handleAddStpBttn = (event) => {
+	if (event.target.matches('button.add-step-button')) {
+		const newSteps = event.target;
+		const newStp = `
+		<li>
+			<input type="text" id="steps" name="steps"><br>
+		</li>
+		`;
+
+		newSteps.insertAdjacentHTML('beforebegin', newStp);
+	}
+};
+
+window.addEventListener('click', handleAddStpBttn);
